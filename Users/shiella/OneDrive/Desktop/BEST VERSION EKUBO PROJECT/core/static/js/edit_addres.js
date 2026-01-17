e55964be37410b2fa165c172391e24ba27629781
Jayson Pardilla <pardillajayson004@gmail.com>
2026-01-18 01:53:45 +0800

    Delete Users/shiella/OneDrive/Desktop directory

--- Users/shiella/OneDrive/Desktop/BEST VERSION EKUBO PROJECT/core/static/js/edit_addres.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-    $("#interactive-form").on("submit", function () {
-        $("#submit-btn").text("Submitting...").addClass("btn-loading");
-    });
-});
--- Users/shiella/OneDrive/Desktop/EKUBO 1/core/chat/context_processors.py
+++
@@ -1,5 +0,0 @@
-from products.models import Business  # Import your Store model
-
-def store_context(request):
-    stores = Business.objects.all()  # Fetch all stores
-    return {'stores': stores}  # Make it available in all templates
